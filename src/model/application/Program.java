928b7f18f674a8d1ca2833b855f2d807c128fb21
RaposoBrendo <raposobrendo@gmail.com>
2026-06-12 15:39:02 -0300

    Seller and Department Dao Interfaces

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -1,4 +1,17 @@
-package model.entities.application;
+package model.application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
+    public static void main(String[] Args){
+
+        Department obj= new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj );
+
+        System.out.println(seller);
+    }
 }
